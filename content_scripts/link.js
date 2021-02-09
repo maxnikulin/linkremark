013898ebb4880915eb55d9b287ecedceb474f3ee
Max Nikulin <manikulin@gmail.com>
2021-02-09 11:54:35 +0700

    content_scripts/link.js: move functions out of try-catch
    Anyway syntax error scope could not be restricted by try-catch,
    so additional indent does not help to make script more resistant
    to accidental mistakes.

--- content_scripts/link.js
+++ content_scripts/link.js
@@ -68,159 +68,159 @@
 		return { value };
 	}
 
-	try {
-		function lrRandomId() {
-			return Math.floor(Math.random()*Math.pow(2, 53));
-		}
+	function lrRandomId() {
+		return Math.floor(Math.random()*Math.pow(2, 53));
+	}
 
-		async function lrSendMessageChrome(msg) {
-			const error = new Error();
-			return new Promise(function(resolve, reject) {
-				try {
-					chrome.runtime.sendMessage(msg, function(response) {
-						const lastError = chrome.runtime.lastError;
-						if (lastError instanceof Error) {
-							reject(lastError);
-						} else if (lastError) {
-							error.message = lastError.message || "lrSendMessage: empty lastError";
-							reject(error);
-						} else {
-							resolve(response);
-						}
-					});
-				} catch (ex) {
-					reject(ex);
+	async function lrSendMessageChrome(msg) {
+		const error = new Error();
+		return new Promise(function(resolve, reject) {
+			try {
+				chrome.runtime.sendMessage(msg, function(response) {
+					const lastError = chrome.runtime.lastError;
+					if (lastError instanceof Error) {
+						reject(lastError);
+					} else if (lastError) {
+						error.message = lastError.message || "lrSendMessage: empty lastError";
+						reject(error);
+					} else {
+						resolve(response);
+					}
+				});
+			} catch (ex) {
+				reject(ex);
+			}
+		});
+	}
+
+	async function lrSendMessage(method, params) {
+		const msg = {method, params};
+		const response = await (
+			typeof browser !== "undefined" ?
+			browser.runtime.sendMessage(msg) : lrSendMessageChrome(msg)
+		);
+		if (response != null && response.result !== undefined) {
+			return response.result;
+		} else if (response != null && response.error) {
+			throw response.error;
+		}
+		throw new Error ("Invalid response object");
+	}
+
+	async function lrSettleAsyncScriptPromise(promiseId, func) {
+		let result;
+		try {
+			result = await func();
+			// lrSendSendMessage for result should be outside of try-catch
+			// since there is no point to report its failure to the background page
+			// using the same (already failed) method.
+		} catch (ex) {
+			lrSendMessage("asyncScript.reject", [ promiseId, lrToObject(ex) ]);
+			throw ex;
+		}
+		lrSendMessage("asyncScript.resolve", [ promiseId, result ]);
+	}
+
+	async function getTargetElement(errorCb) {
+		try {
+			const targetElementId = await lrSendMessage("cache.getTargetElement", []);
+			const menus = typeof browser !== "undefined" ? browser.menus : chrome.menus;
+			if (targetElementId != null) {
+				return menus.getTargetElement(targetElementId);
+			}
+		} catch (ex) {
+			if (errorCb) {
+				errorCb(ex);
+			} else {
+				console.error("LR: getTargetElement: %o", ex);
+			}
+		}
+		return document.activeElement;
+	}
+		
+	function getUrl(node, attr) {
+		const hrefAttr = node.getAttribute(attr);
+		if (!hrefAttr || hrefAttr === "#") {
+			return { error: "LrNoURL" };
+		} else if (hrefAttr.startsWith("javascript:")) {
+			return { value: "javascript:", error: "LrPlaceHolder" };
+		} else if (hrefAttr.startsWith("data:")) {
+			return { value: "data:", error: "LrPlaceHolder" };
+		}
+		return lrNormalize(node[attr]);
+	}
+
+	function addDescriptor(node, array, { attribute, property, key }) {
+		const value = node.getAttribute(attribute);
+		if (value != null) {
+			array.push({...lrNormalize(value), property, key});
+		}
+	}
+
+	function getText(node) {
+		let result = null;
+		try {
+			// TODO more smart cut, avoid other links in siblings
+			result = node.innerText;
+			// TODO try to get text from title and alt elements inside (e.g. images)
+			for (
+				let parentNode = result.parent;
+				result.length < 15 && parentNode !== document.html;
+				parentNode = result.parentNode
+			) {
+				// TODO capture some text around <a> element, not whole parent text
+				if (parentNode.innerText.length > 120) {
+					break;
 				}
+				result = parentNode.innerText;
+			}
+		} catch (ex) {
+			return { error: lrToObject(ex) };
+		}
+		// Allow long enough text if it is link innerText,
+		// otherwise only short context from text around is allowed.
+		return result && lrNormalize(result, TEXT_SIZE_LIMIT);
+	}
+
+	async function lrLinkProperties() {
+		const result = [];
+		function pushWarning(error, key) {
+			result.push({
+				property: 'warning',
+				value: lrToObject(error),
+				key: key || 'lr.link',
 			});
 		}
 
-		async function lrSendMessage(method, params) {
-			const msg = {method, params};
-			const response = await (
-				typeof browser !== "undefined" ?
-				browser.runtime.sendMessage(msg) : lrSendMessageChrome(msg)
-			);
-			if (response != null && response.result !== undefined) {
-				return response.result;
-			} else if (response != null && response.error) {
-				throw response.error;
-			}
-			throw new Error ("Invalid response object");
+		let link = await getTargetElement(error => pushWarning(error, 'lr.link.getTargetElement'));
+		// Original click target could be suitable if link text is too long
+		for (; link != null && link.nodeName != 'A' && link != document.body; link = link.parentNode)
+			;
+		if (link == null || link.nodeName != 'A') {
+			throw new Error(`target element is not a link: ${link && link.nodeName}`);
 		}
-
-		async function lrSettleAsyncScriptPromise(promiseId, func) {
-			let result;
+		result.push({ ...getUrl(link, "href"), property: 'linkUrl', key: 'link.href' });
+		const attrArray = [
+			{ attribute: 'title', property: 'linkTitle', key: 'link.title' },
+			{ attribute: 'download', property: 'linkDownload', key: 'link.download' },
+			{ attribute: 'hreflang', property: 'linkHreflang', key: 'link.hreflang' },
+			{ attribute: 'type', property: 'linkType', key: 'link.type' },
+		];
+		for (const attr of attrArray) {
 			try {
-				result = await func();
-				// lrSendSendMessage for result should be outside of try-catch
-				// since there is no point to report its failure to the background page
-				// using the same (already failed) method.
+				addDescriptor(link, result, attr);
 			} catch (ex) {
-				lrSendMessage("asyncScript.reject", [ promiseId, lrToObject(ex) ]);
-				throw ex;
-			}
-			lrSendMessage("asyncScript.resolve", [ promiseId, result ]);
-		}
-
-		async function getTargetElement(errorCb) {
-			try {
-				const targetElementId = await lrSendMessage("cache.getTargetElement", []);
-				const menus = typeof browser !== "undefined" ? browser.menus : chrome.menus;
-				if (targetElementId != null) {
-					return menus.getTargetElement(targetElementId);
-				}
-			} catch (ex) {
-				if (errorCb) {
-					errorCb(ex);
-				} else {
-					console.error("LR: getTargetElement: %o", ex);
-				}
-			}
-			return document.activeElement;
-		}
-		
-		function getUrl(node, attr) {
-			const hrefAttr = node.getAttribute(attr);
-			if (!hrefAttr || hrefAttr === "#") {
-				return { error: "LrNoURL" };
-			} else if (hrefAttr.startsWith("javascript:")) {
-				return { value: "javascript:", error: "LrPlaceHolder" };
-			} else if (hrefAttr.startsWith("data:")) {
-				return { value: "data:", error: "LrPlaceHolder" };
-			}
-			return lrNormalize(node[attr]);
-		}
-
-		function addDescriptor(node, array, { attribute, property, key }) {
-			const value = node.getAttribute(attribute);
-			if (value != null) {
-				array.push({...lrNormalize(value), property, key});
+				result.push({...attr, error: lrToObject(ex)});
 			}
 		}
-
-		function getText(node) {
-			let result = null;
-			try {
-				// TODO more smart cut, avoid other links in siblings
-				result = node.innerText;
-				// TODO try to get text from title and alt elements inside (e.g. images)
-				for (
-					let parentNode = result.parent;
-					result.length < 15 && parentNode !== document.html;
-					parentNode = result.parentNode
-				) {
-					// TODO capture some text around <a> element, not whole parent text
-					if (parentNode.innerText.length > 120) {
-						break;
-					}
-					result = parentNode.innerText;
-				}
-			} catch (ex) {
-				return { error: lrToObject(ex) };
-			}
-			// Allow long enough text if it is link innerText,
-			// otherwise only short context from text around is allowed.
-			return result && lrNormalize(result, TEXT_SIZE_LIMIT);
+		const textDescriptor = getText(link);
+		if (textDescriptor) {
+			result.push({ ...textDescriptor, property: 'linkText', key: 'link.text' });
 		}
+		return result;
+	}
 
-		async function lrLinkProperties() {
-			const result = [];
-			function pushWarning(error, key) {
-				result.push({
-					property: 'warning',
-					value: lrToObject(error),
-					key: key || 'lr.link',
-				});
-			}
-
-			let link = await getTargetElement(error => pushWarning(error, 'lr.link.getTargetElement'));
-			// Original click target could be suitable if link text is too long
-			for (; link != null && link.nodeName != 'A' && link != document.body; link = link.parentNode)
-				;
-			if (link == null || link.nodeName != 'A') {
-				throw new Error(`target element is not a link: ${link && link.nodeName}`);
-			}
-			result.push({ ...getUrl(link, "href"), property: 'linkUrl', key: 'link.href' });
-			const attrArray = [
-				{ attribute: 'title', property: 'linkTitle', key: 'link.title' },
-				{ attribute: 'download', property: 'linkDownload', key: 'link.download' },
-				{ attribute: 'hreflang', property: 'linkHreflang', key: 'link.hreflang' },
-				{ attribute: 'type', property: 'linkType', key: 'link.type' },
-			];
-			for (const attr of attrArray) {
-				try {
-					addDescriptor(link, result, attr);
-				} catch (ex) {
-					result.push({...attr, error: lrToObject(ex)});
-				}
-			}
-			const textDescriptor = getText(link);
-			if (textDescriptor) {
-				result.push({ ...textDescriptor, property: 'linkText', key: 'link.text' });
-			}
-			return result;
-		}
-
+	try {
 		const promiseId = lrRandomId();
 		// async function does not block execution
 		lrSettleAsyncScriptPromise(promiseId, lrLinkProperties);
