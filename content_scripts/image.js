859e282d0c1d0c211e6d6de3dd979014d147e98d
Max Nikulin <manikulin@gmail.com>
2021-02-09 12:57:05 +0700

    content_scripts/image.js: move function out of try-catch

--- content_scripts/image.js
+++ content_scripts/image.js
@@ -68,129 +68,129 @@
 		}
 	}
 
-	try {
-		function lrRandomId() {
-			return Math.floor(Math.random()*Math.pow(2, 53));
-		}
+	function lrRandomId() {
+		return Math.floor(Math.random()*Math.pow(2, 53));
+	}
 
-		async function lrSendMessageChrome(msg) {
-			const error = new Error();
-			return new Promise(function(resolve, reject) {
-				try {
-					chrome.runtime.sendMessage(msg, function(response) {
-						const lastError = chrome.runtime.lastError;
-						if (lastError instanceof Error) {
-							reject(lastError);
-						} else if (lastError) {
-							error.message = lastError.message || "lrSendMessage: empty lastError";
-							reject(error);
-						} else {
-							resolve(response);
-						}
-					});
-				} catch (ex) {
-					reject(ex);
-				}
+	async function lrSendMessageChrome(msg) {
+		const error = new Error();
+		return new Promise(function(resolve, reject) {
+			try {
+				chrome.runtime.sendMessage(msg, function(response) {
+					const lastError = chrome.runtime.lastError;
+					if (lastError instanceof Error) {
+						reject(lastError);
+					} else if (lastError) {
+						error.message = lastError.message || "lrSendMessage: empty lastError";
+						reject(error);
+					} else {
+						resolve(response);
+					}
+				});
+			} catch (ex) {
+				reject(ex);
+			}
+		});
+	}
+
+	async function lrSendMessage(method, params) {
+		const msg = {method, params};
+		const response = await (
+			typeof browser !== "undefined" ?
+			browser.runtime.sendMessage(msg) : lrSendMessageChrome(msg)
+		);
+		if (response != null && response.result !== undefined) {
+			return response.result;
+		} else if (response != null && response.error) {
+			throw response.error;
+		}
+		throw new Error ("Invalid response object");
+	}
+
+	async function lrSettleAsyncScriptPromise(promiseId, func) {
+		let result;
+		try {
+			result = await func();
+			// lrSendSendMessage for result should be outside of try-catch
+			// since there is no point to report its failure to the background page
+			// using the same (already failed) method.
+		} catch (ex) {
+			lrSendMessage("asyncScript.reject", [ promiseId, lrToObject(ex) ]);
+			throw ex;
+		}
+		lrSendMessage("asyncScript.resolve", [ promiseId, result ]);
+	}
+
+	async function getTargetElement(errorCb) {
+		try {
+			const targetElementId = await lrSendMessage("cache.getTargetElement", []);
+			const menus = typeof browser !== "undefined" ? browser.menus : chrome.menus;
+			if (targetElementId != null) {
+				return menus.getTargetElement(targetElementId);
+			}
+		} catch (ex) {
+			if (errorCb) {
+				errorCb(ex);
+			} else {
+				console.error("LR: getTargetElement: %o", ex);
+			}
+		}
+		// Likely useless, returns `BODY`.
+		// `document.querySelectorAll(":focus")` returns empty node list.
+		// Such fallback works for focusable elements as links however.
+		// console.debug(document.activeElement, document.querySelectorAll(":focus"));
+		return document.activeElement;
+	}
+	
+	function getUrl(node, attr) {
+		const hrefAttr = node.getAttribute(attr);
+		if (!hrefAttr || hrefAttr === "#" || hrefAttr.startsWith("javascript:")) {
+			return { error: "LrNoURL" };
+		} else if (hrefAttr.startsWith("data:")) {
+			return { value: "data:", error: "LrPlaceHolder" };
+		}
+		return lrNormalize(node[attr]);
+	}
+
+	function addDescriptor(node, array, { attribute, property, key }) {
+		const value = node.getAttribute(attribute);
+		if (value != null) {
+			array.push({...lrNormalize(value), property, key});
+		}
+	}
+
+	async function lrImageProperties() {
+		const result = [];
+		function pushWarning(error, key) {
+			result.push({
+				property: 'warning',
+				value: lrToObject(error),
+				key: key || 'lr.image',
 			});
 		}
 
-		async function lrSendMessage(method, params) {
-			const msg = {method, params};
-			const response = await (
-				typeof browser !== "undefined" ?
-				browser.runtime.sendMessage(msg) : lrSendMessageChrome(msg)
-			);
-			if (response != null && response.result !== undefined) {
-				return response.result;
-			} else if (response != null && response.error) {
-				throw response.error;
-			}
-			throw new Error ("Invalid response object");
+		const img = await getTargetElement(error => pushWarning(error, 'lr.image.getTargetElement'));
+		if (img == null || img.nodeName != 'IMG') {
+			// Maybe it is worth checking CSS background-url property
+			throw new Error(`target element is not an image: ${img && img.nodeName}`);
 		}
 
-		async function lrSettleAsyncScriptPromise(promiseId, func) {
-			let result;
+		result.push({ ...getUrl(img, 'src'), property: 'srcUrl', key: 'image.src' });
+		const attrArray = [
+			{ attribute: 'alt', property: 'imageAlt', key: 'image.alt' },
+			{ attribute: 'title', property: 'imageTitle', key: 'image.title' },
+		];
+		for (const attr of attrArray) {
 			try {
-				result = await func();
-				// lrSendSendMessage for result should be outside of try-catch
-				// since there is no point to report its failure to the background page
-				// using the same (already failed) method.
+				addDescriptor(img, result, attr);
 			} catch (ex) {
-				lrSendMessage("asyncScript.reject", [ promiseId, lrToObject(ex) ]);
-				throw ex;
-			}
-			lrSendMessage("asyncScript.resolve", [ promiseId, result ]);
-		}
-
-		async function getTargetElement(errorCb) {
-			try {
-				const targetElementId = await lrSendMessage("cache.getTargetElement", []);
-				const menus = typeof browser !== "undefined" ? browser.menus : chrome.menus;
-				if (targetElementId != null) {
-					return menus.getTargetElement(targetElementId);
-				}
-			} catch (ex) {
-				if (errorCb) {
-					errorCb(ex);
-				} else {
-					console.error("LR: getTargetElement: %o", ex);
-				}
-			}
-			// Likely useless, returns `BODY`.
-			// `document.querySelectorAll(":focus")` returns empty node list.
-			// Such fallback works for focusable elements as links however.
-			// console.debug(document.activeElement, document.querySelectorAll(":focus"));
-			return document.activeElement;
-		}
-		
-		function getUrl(node, attr) {
-			const hrefAttr = node.getAttribute(attr);
-			if (!hrefAttr || hrefAttr === "#" || hrefAttr.startsWith("javascript:")) {
-				return { error: "LrNoURL" };
-			} else if (hrefAttr.startsWith("data:")) {
-				return { value: "data:", error: "LrPlaceHolder" };
-			}
-			return lrNormalize(node[attr]);
-		}
-
-		function addDescriptor(node, array, { attribute, property, key }) {
-			const value = node.getAttribute(attribute);
-			if (value != null) {
-				array.push({...lrNormalize(value), property, key});
+				result.push({...attr, error: lrToObject(ex)});
 			}
 		}
+		return result;
+	}
 
-		async function lrImageProperties() {
-			const result = [];
-			function pushWarning(error, key) {
-				result.push({
-					property: 'warning',
-					value: lrToObject(error),
-					key: key || 'lr.image',
-				});
-			}
-
-			const img = await getTargetElement(error => pushWarning(error, 'lr.image.getTargetElement'));
-			if (img == null || img.nodeName != 'IMG') {
-				// Maybe it is worth checking CSS background-url property
-				throw new Error(`target element is not an image: ${img && img.nodeName}`);
-			}
-
-			result.push({ ...getUrl(img, 'src'), property: 'srcUrl', key: 'image.src' });
-			const attrArray = [
-				{ attribute: 'alt', property: 'imageAlt', key: 'image.alt' },
-				{ attribute: 'title', property: 'imageTitle', key: 'image.title' },
-			];
-			for (const attr of attrArray) {
-				try {
-					addDescriptor(img, result, attr);
-				} catch (ex) {
-					result.push({...attr, error: lrToObject(ex)});
-				}
-			}
-			return result;
-		}
-
+	try {
 		const promiseId = lrRandomId();
 		// async function does not block execution
 		lrSettleAsyncScriptPromise(promiseId, lrImageProperties);
