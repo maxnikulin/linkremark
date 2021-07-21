0e4ec5691c302f5a031ea912cec616e31514d1b1
Max Nikulin <manikulin@gmail.com>
2021-07-21 11:47:29 +0700

    Update copyright year

--- examples/backend-python/lr_webextensions/native_messaging.py
+++ examples/backend-python/lr_webextensions/native_messaging.py
@@ -1,5 +1,5 @@
 
-# Copyright (C) 2020 Max Nikulin
+# Copyright (C) 2020-2021 Max Nikulin
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
