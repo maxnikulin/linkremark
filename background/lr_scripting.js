9aaecb0d5247ab6b02df688ff0a3671c62d97bc4
Max Nikulin <manikulin@gmail.com>
2023-07-11 16:40:17 +0700

    lr_scripting: Review error handling
    Add links to bugs to comments.

--- background/lr_scripting.js
+++ background/lr_scripting.js
@@ -20,28 +20,83 @@
 var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 	const lr_scripting = this;
 
-	lr_scripting._normalizeError = function _normalizeError(error) {
-		if (lr_util.isError(error)) {
-			if (!error.stack) {
-				// TODO strip current function
-				error.stack = new Error().stack;
+	lr_scripting._normalizeError = function _normalizeError(error, refError) {
+		try {
+			if (error == null) {
+				return error;
+			}
+			// Firefox-102 uses JSON serialization for exceptions thrown
+			// by content scripts, so most of fields are unavailable.
+			let isObject;
+			if (
+				lr_util.isError(error)
+				|| (isObject = Object.getPrototypeOf(error) === Object.prototype)
+			) {
+				if (error.message === undefined && isObject && lr_util.has(error, "message")) {
+					// Firefox-102: `Object { message: undefined }`
+					// when a content script `files` is not found.
+					error.message = "Undefined error, likely content script file not found";
+				}
+			} else if (typeof error === "string") {
+				// <https://bugzilla.mozilla.org/1824896>
+				// "WebExtensions scripting.executeScript(): typeof
+				// InjectionResult.error === "string" when InjectionTarget.files
+				// not found"
+				error = new Error(error);
+			} else {
+				console.warn("lr_scripting: unexpected error type", error);
+				error = new TypeError("Unexpected error type " + String(error));
+			}
+
+			// <https://bugzilla.mozilla.org/1825215>
+			// "undefined" string as Error.fileName in exceptions
+			// thrown by scripting.executeScript"
+			if (!error.stack && refError?.stack) {
+				for (const f of ["stack", "fileName", "lineNumber", "columnNumber"]) {
+					const vErr = error[f];
+					const vRef = refError[f];
+					if ((vErr == null || vErr === "") && vRef != null) {
+						error[f] = vRef;
+					}
+				}
 			}
 			return error;
-		} else if (typeof error === "string") {
-			return new Error(error);
-		}
-		try {
-			console.warn("lr_scripting: unexpected error type", error);
-			return new Error(String(error));
 		} catch (ex) {
+			console.error("lr_scripting: failed to normalize error", error);
 			return ex;
 		}
-		return new TypeError("Unsupported error");
 	}
 
-	/** Check lack of permissions for content scripts on particular page
-	 * To avoid confusion with failure of invalid content script.
-	 * `permissions.contains({origins: [url]})` gives false positive for PDF files and reader mode.
+	// <https://bugzilla.mozilla.org/1824901>
+	// "WebExtensions scripting.executeScript() returns [undefined]
+	// array for about:debugging page"
+	//
+	// Since at least Firefox 102 till at least 110
+	// it happense for privileged pages.
+	// The same is for `tabs.executeScript` and e.g. Firefox 93 mv2
+	// where `tabs.executeScript` returns `[undefined]`
+	// for `about:` tabs.
+	lr_scripting._isFirefoxPrivilegedTabResult
+		= function _isFirefoxPrivilegedTabResult(injectionResult) {
+			return injectionResult === undefined && lr_common.isGecko();
+		};
+
+	/** Check for lack of permissions for content scripts on particular page
+	 *
+	 * To avoid confusion with failure of an invalid content script.
+	 *
+	 * Throws an exception when something unexpected happens.
+	 *
+	 * `permissions.contains({origins: [url]})` is not an option.
+	 * It gives false positive for PDF files and reader mode.
+	 * When the `<all_urls>` permission is not granted and the add-on gets
+	 * access through `activeTab`, `permissions.contains` always returns `false`.
+	 * Moreover the method may throw an exception in the case of e.g. `data:`
+	 * URI scheme.
+	 *
+	 * Checking `Error.message` for every possible text is tedious
+	 * (see comments in the function body),
+	 * so just try to inject a simple function.
 	 */
 	lr_scripting.isForbidden = async function isForbidden(target) {
 		if (target == null) {
@@ -59,7 +114,7 @@ var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 			resultArray = await bapi.scripting.executeScript({
 				target: { tabId, frameIds: [ frameId ], allFrames: false, },
 				func: _lrScriptingIsAllowedContent,
-				// Do not do use it, it does not help against hanged scripts
+				// Do not use it, it does not help against hanged scripts
 				// when top tab page has different origin than clicked frame
 				// (especially for PDF file, pages and popups of other extensions, etc).
 				// Instead it causes "Reload page to use this extension" prompt
@@ -68,6 +123,7 @@ var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 			});
 		} catch (ex) {
 			if (ex instanceof TypeError || ex instanceof ReferenceError) {
+				// An error in the code calling the method.
 				throw ex;
 			}
 			// It is usually an `Error` object and checking `message`
@@ -87,39 +143,73 @@ var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 			return true;
 		}
 		if (!Array.isArray(resultArray) || resultArray.length !== 1) {
-			console.warn(
-				"lr_scripting.isForbidden: result is not a single element array",
-				resultArray);
-			return true;
+			console.warn("lr_scripting.isForbidden: Array[1] expected", resultArray);
+			throw new Error("Unexpected executeScript result");
 		}
-		const apiResult = resultArray[0];
-		if (apiResult !== undefined && apiResult?.frameId !== target.frameId) {
+		const injectionResult = resultArray[0];
+		if (injectionResult !== undefined && injectionResult?.frameId !== target.frameId) {
 			console.warn(
 				"lr_scripting.isForbidden: target and result frameId mismatch",
 				target, resultArray);
 		}
-		if (apiResult?.result === _lrScriptingIsAllowedContent()) {
+		if (injectionResult?.result === _lrScriptingIsAllowedContent()) {
 			return false;
 		}
-		if (apiResult === undefined && lr_common.isGecko()) {
-			// Firefox 102 (-?) ... 110 (+?) privileged page like about:debug
-			// The same is for tabs.executeScript and Firefox 93 (-?) (+?)
-			// `tabs.executeScript` for mv2 and e.g. Firefox-93 about: tab
-			// returns `undefined` array element as well.
+		if (lr_scripting._isFirefoxPrivilegedTabResult(injectionResult)) {
 			return true;
 		}
-		const { error } = apiResult;
+		const { error } = injectionResult;
 		if (error != null) {
-			throw lr_scripting._normalizeError(error);
+			const e = lr_scripting._normalizeError(error);
+			if (!lr_util.isError(e)) {
+				// Plain `Object` due to JSON serialization in Firefox-102 ESR.
+				throw new Error(e.message);
+			}
+			throw e;
 		}
-		if (apiResult?.result === null) {
+		if (injectionResult?.result === null) {
 			// Chrome-111 hides exception details, it should not happen though.
-			throw new Error("Unknown content script function error");
+			throw new Error("Content script error not exposed");
 		}
 		console.warn("lr_scripting.isForbidden: unexpected result", result);
 		throw new Error("Unexpected script result");
 	};
 
+	/** Execute content script that should return a `{ result, error, warnings }` object
+	 *
+	 * Handle specific cases of other return values.
+	 *
+	 * May either return an Object `{ result, error, warnings }`
+	 * or throw an exception.
+	 *
+	 * Chrome does not support the `InjectionResult.error` field in the case
+	 * of an exception in the content script,
+	 * it just returns `null` as the `InjectionResult.result` value.
+	 *
+	 * <https://crbug.com/1271527>
+	 * "Propagate errors from scripting.executeScript to InjectionResult"
+	 *
+	 * In content scripts explicitly catch `Error` objects
+	 * and convert them to `Object` values. Despite Firefox uses
+	 * the `structuredClone` algorithm, Firefox-102 ESR can not clone
+	 * any `Error` instances and Firefox-113 has an issue
+	 * with the `DOMException` type.
+	 *
+	 * <https://bugzilla.mozilla.org/1835058>
+	 * "scriting.executeScript throws when cloning DOMException"
+	 *
+	 * Firefox-102 uses JSON serialization for exceptions thrown by
+	 * content scripts.
+	 *
+	 * Chrome uses JSON serialization, so caller receives empty
+	 * objects instead of `Error` instances.
+	 *
+	 * <https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Chrome_incompatibilities#data_cloning_algorithm>
+	 * <https://crbug.com/248548>
+	 * "Extension messaging uses base::Value (JSON)
+	 * serialization but could use WebSerializedScriptValue (structured
+	 * cloning)"
+	 */
 	lr_scripting.executeScript = async function executeScript(target, func, args) {
 		const { tabId, frameId, ...others } = target;
 		try {
@@ -130,7 +220,7 @@ var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 				throw new TypeError("Unspecified frameId");
 			}
 		} catch (ex) {
-			console.warn("lr_scripting.executeScript: invalid target:", ex);
+			Promise.reject(ex);
 		}
 		if (!lr_util.isFunction(func) && !lr_util.isAsyncFunction(func)) {
 			throw new TypeError("Not a function");
@@ -170,44 +260,47 @@ var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 				func, args,
 			});
 		} catch (ex) {
-			if (!ex.stack) {
-				// Unsure if it may happen in Chrome due to
-				// https://crbug.com/1271527
-				// "Propagate errors from scripting.executeScript to InjectionResult"
-				// A potential issue is that Chrome adds `toString()`
-				// result to stack.
-				ex.stack = new Error().stack;
-			}
-			throw ex;
+			throw lr_scripting._normalizeError(ex, ex?.stack || new Error());
 		}
 		if (!Array.isArray(apiResultArray) || apiResultArray.length !== 1) {
-			console.warn("lr_scripting.executeScript: unexpected result", apiResultArray);
-			throw new Error("Unexpected content script result");
+			console.warn("lr_scripting.executeScript: Array[1] expected", apiResultArray);
+			if (!(apiResultArray.length > 0)) {
+				throw new Error("Unexpected content script result");
+			}
 		}
-		const frameResult = apiResultArray[0];
-		if (frameResult === undefined && lr_common.isGecko()) {
+		const injectionResult = apiResultArray[0];
+		if (lr_scripting._isFirefoxPrivilegedTabResult(injectionResult)) {
 			throw new Error("Frame is not accessible for content scripts");
 		}
-		if (frameResult == null) {
+		if (injectionResult == null) {
 			throw new Error("Content script InjectionResult in null");
 		}
-		// It seems script execution is allowed, so return an object,
-		// maybe with `error` field instead of throwing an exception.
-		let { result: scriptResult, error: scriptError } = frameResult;
+		if (injectionResult?.frameId !== target.frameId) {
+			console.warn(
+				"lr_scripting.executeScript: target and result frameId mismatch",
+				target, apiResultArray);
+		}
+		// If `injectionResult.result` is `null` or `undefined` then
+		// return `{ error }` if `injectionResult.error` is not an `Error`
+		// (Firefox-102 ESR that uses JSON serialization).
+		// Otherwise throw an exception.
+		let { result: scriptResult, error: scriptError } = injectionResult;
+		scriptError = lr_scripting._normalizeError(
+			scriptError, scriptError?.stack || new Error());
 		if (scriptError == null && scriptResult === null) {
-			// Chrome
+			// Chrome, see above.
 			scriptError = new Error("Content script error not exposed");
 		}
 		if (scriptResult == null) {
-			return {
-				error: lr_common.errorToObject(lr_scripting._normalizeError(
-					scriptError || new Error("No script result"))),
-			};
+			if (lr_util.isError(scriptError)) {
+				throw scriptError;
+			}
+			return { error: scriptError };
 		}
 		let { result, error, warnings, ...retvalOther } = scriptResult;
 		try {
 			if (scriptError != null) {
-				const errorObject = lr_common.errorToObject(lr_scripting._normalizeError(scriptError));
+				const errorObject = lr_common.errorToObject(scriptError);
 				if (result != null || error != null) {
 					warnings = warnings || [];
 					warnings.push(errorObject);
@@ -216,24 +309,26 @@ var lr_scripting = lr_util.namespace(lr_scripting, function lr_scripting() {
 				}
 			}
 		} catch (ex) {
-			console.warn("lr_scripting: ignore exception", ex);
+			Promise.reject(ex);
 		}
 
 		try {
 			const otherKeys = Object.keys(retvalOther);
 			if (otherKeys.length != 0) {
 				console.warn("lr_scripting: unexpected content script result fields", retvalOther);
-				const errorObject = lr_common.errorToObject(
-					new Error("Unexpected content script result fields: " + otherKeys));
+				const keysError = new TypeError(
+					"Unexpected content script result fields: " + otherKeys);
 				if (result != null || error != null) {
 					warnings = warnings || [];
-					warnings.push(errorObject);
+					warnings.push(lr_common.errorToObject(keysError));
+				} else if (warnings != null) {
+					error = lr_common.errorToObject(keysError);
 				} else {
-					error = errorObject;
+					throw keysError;
 				}
 			}
 		} catch (ex) {
-			console.warn("lr_scripting: ignore exception", ex);
+			Promise.reject(ex);
 		}
 		const retval = {};
 		if (result != null) {
